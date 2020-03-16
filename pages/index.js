852b57b2efb6089bbfb9d9884f111ac1fa65503b
Neel Redkar <neel.redkar@outlook.com>
2020-03-15 17:46:37 -0700

    add deaths and recovered

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Box, Flex, Text, Heading, Card } from "rebass";
-import {Input, Select} from "@rebass/forms"
+import { Input, Select } from "@rebass/forms"
 import {
     PieChart,
     Pie,
@@ -58,36 +58,48 @@ export default class Index extends React.Component {
                         <Bar dataKey="value" fill="grey" />
                     </BarChart>
                 </Flex>
-                <hr width="90vw"/>
+                <hr width="90vw" />
                 <Flex flexDirection="column">
                     <Box width="30vw" m="auto">
-                        <Select defaultValue="Italy" sx={{borderRadius:"10px"}} onChange={e => this.setState({name:e.target.value})}>
-                            {Object.keys(this.state.county).map(v => <option key={v}>{v}</option> )}
+                        <Select defaultValue="Italy" sx={{ borderRadius: "10px" }} onChange={e => this.setState({ name: e.target.value })}>
+                            {Object.keys(this.state.county).map(v => <option key={v}>{v}</option>)}
                         </Select>
                     </Box>
                     <Box width="80vw" height="50vh" m="auto">
-                    <ResponsiveContainer minHeight={100} width="100%" height="100%">
-                        <AreaChart style={{ margin: "auto" }}
-                            data={
-                                this.state.county && this.state.county[this.state.name] ? Object.entries(this.state.county[this.state.name].history).sort((a, b) => {
-                                    return moment(a[0], 'MM/DD/YY').unix() - moment(b[0], 'MM/DD/YY').unix();;
-                                }).map(v => {
-                                    return { "Confirmed Cases": v[1], day: v[0] };
-                                }) : []
-                            }
-                            margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
-                        >
-                            <XAxis dataKey="day" />
-                            <YAxis />
-                            <CartesianGrid strokeDasharray="3 3" />
-                            <Tooltip />
-                            <Area
-                                type="monotone"
-                                dataKey="Confirmed Cases"
-                                fillOpacity={0.7}
-                                fill="#8884d8"
-                            />
-                        </AreaChart>
+                        <ResponsiveContainer minHeight={100} width="100%" height="100%">
+                            <AreaChart style={{ margin: "auto" }}
+                                data={
+                                    this.state.county && this.state.county[this.state.name] ? Object.entries(this.state.county[this.state.name].history).sort((a, b) => {
+                                        return moment(a[0], 'MM/DD/YY').unix() - moment(b[0], 'MM/DD/YY').unix();;
+                                    }).map(v => {
+                                        return { "Confirmed Cases": v[1].confirmed, "Recovered": v[1].recovered, "Deaths": v[1].deaths, day: v[0] };
+                                    }) : []
+                                }
+                                margin={{ top: 10, right: 30, left: 0, bottom: 0 }}
+                            >
+                                <XAxis dataKey="day" />
+                                <YAxis />
+                                <CartesianGrid strokeDasharray="3 3" />
+                                <Tooltip />
+                                <Area
+                                    type="monotone"
+                                    dataKey="Confirmed Cases"
+                                    fillOpacity={0.7}
+                                    fill="#8884d8"
+                                />
+                                <Area
+                                    type="monotone"
+                                    dataKey="Recovered"
+                                    fillOpacity={0.7}
+                                    fill="hsl(109.5, 95.5%, 43.5%)"
+                                />
+                                <Area
+                                    type="monotone"
+                                    dataKey="Deaths"
+                                    fillOpacity={0.7}
+                                    fill="hsl(351.6, 79.4%, 52.4%)"
+                                />
+                            </AreaChart>
                         </ResponsiveContainer>
                     </Box>
                 </Flex>
@@ -97,10 +109,16 @@ export default class Index extends React.Component {
     }
     async componentWillMount() {
         var data = await (await fetch("https://9a4edec9.ngrok.io/all")).json();
-        console.log(data);
+        let locations = {}
+        Object.keys(data.confirmed.locations).map(p => {
+            locations[p] = { history: {} }
+            Object.keys(data.confirmed.locations[p].history).map(d => {
+                locations[p].history[d] = { confirmed: data.confirmed.locations[p].history[d], deaths: data.deaths.locations[p].history[d], recovered: data.recovered.locations[p].history[d] }
+            })
+        })
         this.setState({
             latest: data.latest,
-            county: data.confirmed.locations
+            county: locations
         });
     }
 }
